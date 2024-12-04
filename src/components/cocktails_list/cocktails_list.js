b94a63008cd293b670f0dd81e544d5eb53a1722a
Viktoria Björklund <vibjo@kth.se>
2024-12-04 09:25:41 +0100

    API call for details

--- src/components/cocktails_list/cocktails_list.js
+++ src/components/cocktails_list/cocktails_list.js
@@ -1,15 +1,41 @@
 import React from 'react';
-
+import DetailsPage from '../../views/details_page';
+import { useState } from 'react';
+const API_KEY = process.env.REACT_APP_DRINK_GENERATOR_API_KEY;
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': API_KEY,
+		'x-rapidapi-host': 'the-cocktail-db.p.rapidapi.com'
+	}
+};
 const CocktailsList = ({ results }) => {
-    console.log("reslts", results);
+    const [Drink_details, setDrink_details] = useState([]);
+
+    const inspectDrink = async (idDrink) => {
+        console.log("ingredient_id", idDrink);
+        const url = `https://the-cocktail-db.p.rapidapi.com/lookup.php?i=${idDrink}`;
+        try {
+            const response = await fetch(url, options);
+            console.log("url", url);
+            const result = await response.text();
+            setDrink_details(result)
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     return (
             <div className='items-center'>
                 {typeof results === 'object' && results.length > 0 ? (
                     <>
-                        <div className='flex flex-wrap gap-8 ml-20 mt-16 mb-16'>
+                        <div className='flex flex-wrap gap-8 ml-20 mt-16 mb-16'
+
+                        >
                             {results.map((result, index) => (
-                                <div key={index} className="w-40 flex flex-col items-center transform transition-transform duration-200 hover:scale-110">
+                                <div key={index} className="w-40 flex flex-col items-center transform transition-transform duration-200 hover:scale-110"
+                                onClick={() => inspectDrink(result.idDrink)}
+                                >
                                     <img
                                         src={result.strDrinkThumb}
                                         alt="Drink image"
@@ -23,6 +49,10 @@ const CocktailsList = ({ results }) => {
                 ) : results === 'None Found' ? (
                     <p className='text-2xl'>No results found</p>
                 ) : null}
+        <div>
+            <DetailsPage drink_details={Drink_details}/>
+    
+        </div>
             </div>
     );
 };
---
+++ src/views/details_page.js
@@ -0,0 +1,15 @@
+import CocktailsList from "../components/cocktails_list/cocktails_list";
+
+const DetailsPage = ({ drink_details }) => {
+    console.log("drink_details", drink_details);
+
+
+    return (
+        <div class="italiana-regular">
+
+            
+        </div>
+    );
+};
+
+export default DetailsPage;
